f58870b9d7cc5f22f97739763f8f394f5a39e881
nassir <nassirmz@gmail.com>
2016-03-08 12:01:14 -0800

    Add rabbit as rotating dancer

---
+++ src/rotateDancer.js
@@ -0,0 +1,16 @@
+var RotateDancer = function(top, left, timeBetweenSteps){
+  Dancer.call(this, top, left, timeBetweenSteps);
+  this.left = left;
+  this.top = top;
+  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+  // so we must keep a copy of the old version of this function
+};
+
+RotateDancer.prototype = Object.create(Dancer.prototype);
+RotateDancer.prototype.constructor = RotateDancer;
+RotateDancer.prototype.step = function() {
+  
+  Dancer.prototype.step.call(this);
+    this.$node.addClass("rotate").addClass("bunny");
+  // animate({width: "20%", opacity: 0.4, borderWidth: "10px"}, 1500)
+};
