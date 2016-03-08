8de348a4d8f9d976a3a6327234948e98d00e2379
nassir <nassirmz@gmail.com>
2016-03-08 12:00:44 -0800

    Add cat a bouncy object

---
+++ src/bouncyDancer.js
@@ -0,0 +1,14 @@
+var BouncyDancer = function(top, left, timeBetweenSteps){
+  Dancer.call(this, top, left, timeBetweenSteps);
+  this.top= top;
+  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+  // so we must keep a copy of the old version of this function
+};
+
+BouncyDancer.prototype = Object.create(Dancer.prototype);
+BouncyDancer.prototype.constructor = BouncyDancer;
+BouncyDancer.prototype.step = function() {
+  
+  Dancer.prototype.step.call(this);
+  this.$node.addClass("bouncy").animate( {top: this.top-50}, 200).animate({top: this.top}, 200);
+};
