f3076a4ed0d0e33fea4c28b6cca81aa0727cd279
nassir <nassirmz@gmail.com>
2016-03-08 12:02:23 -0800

    Add Bear as a blinky dancer

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -10,5 +10,5 @@ BlinkyDancer.prototype.constructor = BlinkyDancer;
 BlinkyDancer.prototype.step = function() {
   
   Dancer.prototype.step.call(this);
-  this.$node.toggle();
+  this.$node.addClass("blinky").toggle();
 };
